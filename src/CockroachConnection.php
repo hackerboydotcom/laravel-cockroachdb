ce0d45f05b2e0279dba050630d1c76bf0c6cf2ed
HackerBoy <admin@hackerboy.com>
2019-09-28 01:44:59 +0700

    + fix update query + fix select for update query

--- src/CockroachConnection.php
+++ src/CockroachConnection.php
@@ -11,6 +11,76 @@ use HackerBoy\LaravelCockroachDB\Grammar\Schema\CockroachGrammar as SchemaGramma
 
 class CockroachConnection extends PostgresConnection
 {
+        /**
+     * Run a select statement against the database.
+     *
+     * @param  string  $query
+     * @param  array  $bindings
+     * @param  bool  $useReadPdo
+     * @return array
+     */
+    public function select($query, $bindings = [], $useReadPdo = true)
+    {
+        // For update is not supported because cockroachdb has no lock
+        // https://github.com/cockroachdb/cockroach/issues/6583
+        if (preg_match('/for\supdate$/i', $query)) {
+            $query = str_replace('for update', '', $query);
+        }
+
+        return $this->run($query, $bindings, function ($query, $bindings) use ($useReadPdo) {
+            if ($this->pretending()) {
+                return [];
+            }
+            // For select statements, we'll simply execute the query and return an array
+            // of the database result set. Each element in the array will be a single
+            // row from the database table, and will either be an array or objects.
+            $statement = $this->prepared($this->getPdoForSelect($useReadPdo)
+                              ->prepare($query));
+            $this->bindValues($statement, $this->prepareBindings($bindings));
+            $statement->execute();
+            return $statement->fetchAll();
+        });
+    }
+
+    /**
+     * Run an SQL statement and get the number of rows affected.
+     *
+     * @param  string  $query
+     * @param  array   $bindings
+     * @return int
+     */
+    public function affectingStatement($query, $bindings = [])
+    {
+        if (preg_match('/^update/i', $query)) {
+
+            preg_match_all('/"[a-z0-9_]+"\./', $query, $matches);
+            $matches = $matches[0];
+
+            foreach ($matches as $match) {
+                $query = str_replace($match, '', $query);
+            }
+
+            $query = str_replace('"', '', $query);
+
+        }
+
+        return $this->run($query, $bindings, function ($query, $bindings) {
+            if ($this->pretending()) {
+                return 0;
+            }
+            // For update or delete statements, we want to get the number of rows affected
+            // by the statement and return that back to the developer. We'll first need
+            // to execute the statement and then we'll use PDO to fetch the affected.
+            $statement = $this->getPdo()->prepare($query);
+            $this->bindValues($statement, $this->prepareBindings($bindings));
+            $statement->execute();
+            $this->recordsHaveBeenModified(
+                ($count = $statement->rowCount()) > 0
+            );
+            return $count;
+        });
+    }
+
     /**
      * Get the default query grammar instance.
      *
